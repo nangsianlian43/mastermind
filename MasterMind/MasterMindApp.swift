f4a19d86b08f5d6b8f48fa3f6aae166d38944e40
Nang <nangsianlian43@gmail.com>
2024-11-06 15:10:41 -0600

    Initial Commit

---
+++ MasterMind/MasterMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterMindApp.swift
+//  MasterMind
+//
+//  Created by Nang Lian on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MasterMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MasterMindTests/MasterMindTests.swift
@@ -0,0 +1,17 @@
+//
+//  MasterMindTests.swift
+//  MasterMindTests
+//
+//  Created by Nang Lian on 11/6/24.
+//
+
+import Testing
+@testable import MasterMind
+
+struct MasterMindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
